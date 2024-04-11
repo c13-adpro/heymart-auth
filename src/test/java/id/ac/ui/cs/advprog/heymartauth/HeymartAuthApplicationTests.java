94b1ee04ca21bec1f46cf308d5a82caf4d30c258
juankhusuma <juan.d.khusuma@gmail.com>
2024-04-12 00:30:41 +0700

    init

---
+++ src/test/java/id/ac/ui/cs/advprog/heymartauth/HeymartAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.heymartauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeymartAuthApplicationTests {
+
+	@Test
+		void contextLoads() {
+	}
+
+}
