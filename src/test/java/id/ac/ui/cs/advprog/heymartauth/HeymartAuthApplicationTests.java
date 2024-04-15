1f7b900225faf8afd4879258ecdfb161f73e9a61
juankhusuma <juan.d.khusuma@gmail.com>
2024-04-16 01:51:51 +0700

    Modified HeymartAuthApplicationTests

--- src/test/java/id/ac/ui/cs/advprog/heymartauth/HeymartAuthApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/heymartauth/HeymartAuthApplicationTests.java
@@ -3,7 +3,7 @@ package id.ac.ui.cs.advprog.heymartauth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = HeymartAuthApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class HeymartAuthApplicationTests {
 
     @Test
